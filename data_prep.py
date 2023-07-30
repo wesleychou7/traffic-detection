45c67a95c88ece783e087d129a8d11cc6c8fdfbc
Wesley Chou <wesleychou9851@gmail.com>
2023-07-30 11:50:53 -0700

    info

--- data_prep.py
+++ data_prep.py
@@ -2,9 +2,6 @@ import os
 import random
 import shutil
 
-# TODO: 
-# - randomly remove some images in each folder in gtsrb-small and move to test dataset
-# - create a function that loads gtsrb-small into ImageFolder
 
 def walk_through_dir(dir_path):
   for dirpath, dirnames, filenames in os.walk(dir_path):
@@ -25,6 +22,7 @@ if __name__=="__main__":
    
     # directory = "gtsrb"
     # walk_through_dir(directory)
+    
     get_test_samples("gtsrb-small/train/0", "gtsrb-small/test/0")
     get_test_samples("gtsrb-small/train/27", "gtsrb-small/test/27")
     get_test_samples("gtsrb-small/train/37", "gtsrb-small/test/37")
@@ -37,3 +35,11 @@ if __name__=="__main__":
 # '0': '20_speed'              - 210
 # '27': 'attention_pedestrian' - 240
 # '37': 'turn_straight_left'   - 210
+
+# There are 0 directories and 24 images in 'gtsrb-small/test/27'.
+# There are 0 directories and 21 images in 'gtsrb-small/test/0'.
+# There are 0 directories and 21 images in 'gtsrb-small/test/37'.
+
+# There are 0 directories and 216 images in 'gtsrb-small/train/27'.
+# There are 0 directories and 189 images in 'gtsrb-small/train/0'.
+# There are 0 directories and 189 images in 'gtsrb-small/train/37'.
